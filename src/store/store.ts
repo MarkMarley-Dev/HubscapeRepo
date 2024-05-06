6ff2400a38249f3556467b9051eb2dec6d23491f
MarkMarley-Dev <markmarley19911@gmail.com>
2024-05-06 19:47:15 +0100

    Initial store implementation and minor style changes

---
+++ src/store/store.ts
@@ -0,0 +1,18 @@
+import {create} from 'zustand';
+import { DayType, mockSchedule } from '../data/MockData';
+
+
+type StoreState = {
+    schedule: DayType[];
+    moveVisit: (dayIndex: number, fromIndex: number, toIndex: number) => void;
+};
+
+export const useStore = create<StoreState>((set) => ({
+    schedule: mockSchedule, 
+    moveVisit: (dayIndex, fromIndex, toIndex) => set((state) => {
+        const newSchedule = [...state.schedule];
+        const movedVisit = newSchedule[dayIndex].visits.splice(fromIndex, 1)[0];
+        newSchedule[dayIndex].visits.splice(toIndex, 0, movedVisit);
+        return { schedule: newSchedule };
+    }),
+}));
